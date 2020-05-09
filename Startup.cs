e75737c039e4fe89897cb997acffc862afaff423
danny10846 <danny10846@gmail.com>
2020-05-09 14:37:47 +0100

    Added basic website files to wwwroot. Starting on the actual MVC components now.

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace DutchTreat {
     public class Startup {
@@ -21,8 +15,10 @@ namespace DutchTreat {
 
             app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseNodeModules();
 
-            
+
+
         }
     }
 }
---
+++ wwwroot/js/index.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    var x = 0;
+    var s = "";
+
+    console.log("Hello!");
+
+
+    //jQuery allows us to get elements within our page easier
+    var theForm = $("#theForm");
+    //gives us a collection as a result, executes methods on the entire collection
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    //Gets us the list items inside the product-props
+    //The 'this' grabs us the item we clicked on within the list
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You've clicked on " + $(this).text());
+    });
+
+    //jQuery objects conventionally start with dollar sign $
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    //When we click the login label, it will hide/show the form
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(500);
+    });
+
+});
